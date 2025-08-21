7fb7d750b13cd5ff50643229209b31e5f1a1d3dc
AllenChang7 <allenychang2003@gmail.com>
2025-08-21 17:59:03 -0400

    update: new cover image

--- app/page.tsx
+++ app/page.tsx
@@ -4,8 +4,8 @@ import { useState, useEffect } from "react";
 import Image from "next/image";
 import CoverImage from "../public/images/caroll-creek/caroll-creek-1.jpg";
 import CoverImage2 from "../public/images/national-headquarter/national-hq-1.png";
-import CoverImage3 from "../public/images/7272-wisconsin-avenue/wisconsin-1.jpg";
-import CoverImage4 from "../public/images/7607-old-georgetown/old-georgetown-1.png";
+import CoverImage3 from "../public/images/caroll-creek/caroll-creek-4.jpg";
+import CoverImage4 from "../public/images/7272-wisconsin-avenue/wisconsin-1.jpg";
 import News from "@/app/components/News";
 
 export default function Home() {
