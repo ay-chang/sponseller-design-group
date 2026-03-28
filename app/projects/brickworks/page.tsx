52f2ae0ff973c1f7414fd5bf52f7a0e5163d9836
AllenChang7 <allenychang2003@gmail.com>
2026-03-28 19:07:51 -0400

    changes

--- app/projects/brickworks/page.tsx
+++ app/projects/brickworks/page.tsx
@@ -3,7 +3,6 @@ import Brickworks1 from "@/public/images/brickworks/brickworks-1.png";
 import Brickworks4 from "@/public/images/brickworks/brickworks-4.jpg";
 import Brickworks5 from "@/public/images/brickworks/brickworks-5.jpg";
 import Brickworks6 from "@/public/images/brickworks/brickworks-6.jpg";
-import Brickworks7 from "@/public/images/brickworks/brickworks-7.jpg";
 
 const Brickworks = () => {
     return (
@@ -11,7 +10,7 @@ const Brickworks = () => {
             <ProjectSkeleton
                 Title="Brickworks"
                 Description="Sponseller Design Group have been awarded the master plan and execution of a 40-unit master plan for affordable housing on a 17-acre site in Belington, West Virginia.  The client group is a national partnership focused on providing affordable single-family homes nationally as part of their effort to increase access to housing for all incomes. The architects will work with pre-fabrication manufacturers in the design forward execution of the housing from housing for singles and the elderly, and families."
-                Images={[Brickworks1, Brickworks4, Brickworks5, Brickworks6, Brickworks7]}
+                Images={[Brickworks1, Brickworks4, Brickworks5, Brickworks6]}
             />
         </div>
     );
