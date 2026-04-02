ef1783672c7e3556734d4d0482c9c2f09959f760
AllenChang7 <allenychang2003@gmail.com>
2026-04-02 14:19:13 -0400

    changed peoples page

--- app/people/page.tsx
+++ app/people/page.tsx
@@ -28,17 +28,15 @@ const People = () => {
                     </p>
                     <p className="text-sm sm:text-md leading-relaxed pb-6 sm:pb-8">
                         Sponseller Design Group was founded in the spring of 2024 by principals
-                        Robert M. Sponseller FAIA, Jennifer Chen AIA, and Huan Chou Chang AIA.
-                        Three colleagues that have been working together for over 15 years in a
-                        previous office in Washington DC.
+                        Robert M. Sponseller FAIA, and Jennifer Chen AIA. Two colleagues that
+                        have been working together for over 15 years in a previous office in
+                        Washington DC.
                     </p>
                     <p className="text-sm sm:text-md leading-relaxed">
                         Robert Sponseller FAIA worked as lead design principal on some of the
-                        most significant sites in Washington DC and surrounding areas. Huan
-                        Chou Chang is an experienced project manager with expertise in all
-                        phases of design and construction. Jennifer Chen is a design team
-                        leader guiding the development and realization of numerous complex
-                        multiple building projects in the region.
+                        most significant sites in Washington DC and surrounding areas. Jennifer
+                        Chen is a design team leader guiding the development and realization of
+                        numerous complex multiple building projects in the region.
                     </p>
                 </div>
             </div>
